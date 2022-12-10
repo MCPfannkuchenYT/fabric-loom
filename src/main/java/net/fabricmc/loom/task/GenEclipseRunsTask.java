32c0f9e152ff10309ca900c5257c899cb9c57cd7
Pfannekuchen <gamesupport@mgnet.work>
2022-12-10 17:04:50 +0100

    Generate launch configs in non-root projects

--- src/main/java/net/fabricmc/loom/task/GenEclipseRunsTask.java
+++ src/main/java/net/fabricmc/loom/task/GenEclipseRunsTask.java
@@ -43,10 +43,6 @@ public class GenEclipseRunsTask extends AbstractLoomTask {
 		LoomGradleExtension extension = getExtension();
 
 		for (RunConfigSettings settings : extension.getRunConfigs()) {
-			if (!settings.isIdeConfigGenerated()) {
-				continue;
-			}
-
 			String name = settings.getName();
 
 			File configs = new File(getProject().getProjectDir(), eclipseModel.getProject().getName() + "_" + name + ".launch");
